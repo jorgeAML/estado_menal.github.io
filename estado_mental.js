af9cf8d6c40f4ec059d75953f9cbda4b3e7ee494
Jorge aml <martjorge8@gmail.com>
2016-09-30 22:46:42 -0600

    Add files via upload

---
+++ estado_mental.js
@@ -0,0 +1,62 @@
+//usa tu capacidad cognitiva!
+"Estado mental"
+"estado mental".length 
+//confirm("Listo para la primera parte de Estado mental?")
+//prompt("Que capitulo de Mosby es?")
+23 > 10 
+console.log(23*11);
+console.log("estado mental capitulo 4");
+console.log("estado mental capitulo 4".length > 22);
+console.log("La educacion superior se ha relacionado con una memoria y funcion cognitiva mejores en etapas avanzadas!".length <= 50);
+if ("lobulo frontal".length  <= 20) {
+	console.log("capacidad de concentracion y a la memoria");
+}
+if (7-50 === 43) {
+	console.log("No tienes depresion")
+} else {
+	console.log("Enfermedad cerebral difusa")
+}
+if (50 + 8 === 58) {
+	console.log("sencilla operacion aritmetica")
+} else {
+	console.log("Enfermedad cerebral difusa")
+}
+if ("plato".length <= 5) {
+	console.log("inteligencia media")
+} else {console.log("Lesion del hemisferio cerebral izquierdo")}
+console.log(16 % 4)
+console.log(16 % 3)
+console.log(16 % 5)
+console.log(16 % 9)
+if ("MUNDO".length === 5) {
+	console.log("capacidad de escritura = buena")
+} else {console.log("demencia o da;o de lobulo parietal")}
+if ("triangulo".length >= 3) {console.log("figura geometrica")} else {"Neuropatia periferica"}
+console.log("dibujar casa".substring(0,11));
+if ("mujer".length === 5) {console.log("pintarse los labios")} else {console.log("ha de ser hombre!")}
+if ("hombre".length === 6) {console.log("peine el pelo")} else {console.log("ha de ser mujer!")}
+console.log("apraxia".substring(0,10));
+console.log("apraxia = trastorno mental".substring(0,27));
+var myage = 22;
+console.log(myage);
+var objetos1 = "lampara,coche,radio,llanta";
+console.log(objetos1);
+var semana = "Lunes, Martes, Miercoles, Jueves, Viernes";
+console.log(semana.substring(0,24));
+var mundo = "M-U-N-D-O";
+mundo = "O-D-N-U-M";
+console.log(mundo);
+var afasia = "afasia de broca expresiva";
+if (afasia === "de aceptale a buena") {console.log("expresiva")} else {console.log("sin afasia")}
+	console.log(afasia);
+if ("aceptable".length === 9) {console.log(afasia)} else {console.log("sujeto sin afasia")};
+	if ("excelente".length < 10) {console.log("sin afasia")} else {console.log("con afasia")};
+confirm("la siguiente pregunta va relacionado a la comprension de palabras!")
+var afasia2 = prompt("El sujeto puede tener afasia de Wernicke o de broca escriba los siguiente para cualquier de los dos casos: A--no relaciona palabras B--aceptable");
+if (afasia2 === "no relaciona palabras") {console.log("afasia de Wernicke")} else {console.log("Puede que no tenga afasia")};
+if (afasia2 === "aceptable") {console.log("afasia de broca")} else {console.log("Puede que no tenga afasia")};
+var afasia3 = function(name) {console.log('no dice palabras o dice pocas ' + name);
+console.log('habla fluido pero puede resultar incomprensible ' + name)};
+
+afasia3("afasia global");
+afasia3("afasia de Wernicke");
