4cdd1e08731ddbeda375794cbb67289522681734
Jorge aml <martjorge8@gmail.com>
2016-10-02 12:33:45 -0600

    Update estado_mental.js

--- estado_mental.js
+++ estado_mental.js
@@ -55,8 +55,81 @@ confirm("la siguiente pregunta va relacionado a la comprension de palabras!")
 var afasia2 = prompt("El sujeto puede tener afasia de Wernicke o de broca escriba los siguiente para cualquier de los dos casos: A--no relaciona palabras B--aceptable");
 if (afasia2 === "no relaciona palabras") {console.log("afasia de Wernicke")} else {console.log("Puede que no tenga afasia")};
 if (afasia2 === "aceptable") {console.log("afasia de broca")} else {console.log("Puede que no tenga afasia")};
-var afasia3 = function(name) {console.log('no dice palabras o dice pocas ' + name);
-console.log('habla fluido pero puede resultar incomprensible ' + name)};
+//var afasia3 = function(name) {console.log('no dice palabras o dice pocas ' + name);
+//console.log('habla fluido pero puede resultar incomprensible ' + name)};
 
-afasia3("afasia global");
-afasia3("afasia de Wernicke");
+//afasia3("afasia global");
+//afasia3("afasia de Wernicke");
+var lenguaje = function(elemento) {
+	var val = elemento + 2;
+	console.log(val);
+	console.log("Utiliza una o dos palabras");
+
+};
+lenguaje(10);
+
+var lenguaje2 = function(elemento){
+	var val = elemento + 12;
+	console.log(val);
+	console.log("Aumento de palabras cada mes");
+};
+lenguaje2(12);
+
+var lenguaje3 = function(meses) {
+	prompt('Utiliza dos o tres palabras juntas ' + meses);
+	};
+lenguaje3('a los cuantos meses? ' + 'entre 12 a 24 ' + 'entre 24 a 36');
+
+var lenguaje4 = function(elemento){
+	console.log(elemento);
+
+}
+lenguaje4("Las frases pueden ser de 4 o mas palabras cuando tiene  36 a 48 meses");
+
+var timesTwo = function(number){
+	return number * 2;
+};
+var newNumber = timesTwo(67);
+console.log(newNumber);
+
+var lenguaje5 = function(number){
+	return number * 2;
+
+};
+if (lenguaje5(48) === 96) {console.log("pronuncia debidamente la mayor parte de los sonidos a los 48 a 60 meses");} else {console.log("Las frases pueden ser de de de 4 o mas palabras");}
+
+var medida = function(argument) {
+	return argument / 3;
+};
+if (medida(18) === 6) {console.log("anchura de codo de hombre 6 a los 18 anios")} else {console.log("revisar medidas pueda ser incorrecto")}
+if (medida(24) - 1 === 7.67) {console.log("anchura de codo de hombre 7.6")} else {console.log("anchura de codo de hombre 7.6 a los 24 anios")}
+var medidaf = function (argument) {
+	return argument / 3.2;
+};
+if (medidaf(18) === 5.625) {console.log("anchura de codo de mujer 5.625")} else {console.log("error")};
+var medidaf24 = function (argument) {
+	return argument / 3.7;
+
+
+};
+
+if (medidaf24(24) === 6.48) {console.log("anchura de codo de mujer 6.48")} else {console.log("error")};
+
+var medida2536 = "De 6.7 a 7.8 en hombres";
+var medidaH2536 = function () {
+	console.log(medida2536);
+}
+console.log(medida2536);
+
+var my_number = 7
+var timesDos = function (number) {
+	var my_number = number * 2;
+	console.log("inside is: ");
+	console.log(my_number);
+}
+console.log("outside is: ");
+console.log(my_number);
+timesDos(7);
+
+
+
